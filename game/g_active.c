fd18fa30b28717cd1b7b56fd8d2eb100f2c52659
TheSil <sildak@gmail.com>
2015-09-08 20:22:13 +0200

    Fixed #92

--- game/g_active.c
+++ game/g_active.c
@@ -3769,6 +3769,11 @@ void ClientEndFrame( gentity_t *ent ) {
             ent->pain_debounce_time += time_delta;
             ent->client->ps.fd.forcePowerRegenDebounceTime += time_delta;
 
+			if ( ent->client->tempSpectate )
+			{
+				ent->client->tempSpectate += time_delta;
+			}
+
 			// update force powers durations
 			for(i=0;i < NUM_FORCE_POWERS;++i) {
 				if (ent->client->ps.fd.forcePowerDuration[i])
